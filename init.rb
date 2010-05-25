26c2861e71702892e6183f09e288558e00df70d2
Cyrill Sizov <rphx@phxwork.(none)>
2010-05-25 11:32:32 +0700

    increase version

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_monthchart do
   name 'Redmine Monthchart plugin'
   author 'Cyrill Sizov'
   description 'Monthly sent time plugin for Redmine'
-  version '0.0.1'
+  version '0.0.3'
 
   menu :top_menu, :monthchart, { :controller => 'calendar', :action => 'index' }, :caption => 'Month Chart',
    :if => Proc.new{User.current.allowed_to?(:log_time, nil, :global => true)} 
